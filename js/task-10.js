0f8d9d928ee10570834cc79bf59bf5728a828169
d3monw1ngs <tezzielumsit87@gmail.com>
2024-02-04 09:56:39 +0800

    update files

--- js/task-10.js
+++ js/task-10.js
@@ -5,8 +5,19 @@ function getRandomHexColor() {
 }
 
 function createBoxes() {
-  const boxCount = document.getElementById('boxCount').value;
-  const boxesContainer = document.getElementById('boxes');
+  let boxCount = document.getElementById('boxCount').value;
+  let boxesContainer = document.getElementById('boxes');
 
-  
+  boxesContainer.innerHTML = "";
+
+  let baseSize = 30;
+  for(let i = 1; i <= boxCount; i++) {
+    let box = document.createElement("div");
+    box.className = "box";
+    box.style.width = baseSize + "px";
+    box.style.height = baseSize + "px";
+    box.style.backgroundColor = getRandomHexColor();
+    boxesContainer.appendChild(box);
+  }
+
 }
